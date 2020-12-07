5512a535831030d9f14878a9b01116f7d05c0176
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-07 17:39:40 +0545

    [Updated] models for categories

--- portfolio/blogs/models.py
+++ portfolio/blogs/models.py
@@ -5,16 +5,16 @@ from django.utils import timezone
 # Blog model for portfolio
 class Blog(models.Model):
     blog_category = [
-        ('P', 'Programming'),
-        ('T', 'Travel'),
-        ('E', 'Education'),
-        ('O', 'Others')
+        ('Programming', 'Programming'),
+        ('Travel', 'Travel'),
+        ('Education', 'Education'),
+        ('Others', 'Others')
     ]
-    category = models.CharField(max_length=1, choices=blog_category)
+    category = models.CharField(max_length=15, choices=blog_category)
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     post = models.TextField()
-    thumbnail = models.ImageField(upload_to='blog_thumbnails', default='blog.jpg')
+    thumbnail = models.ImageField(upload_to='blog_thumbnails', default='blog_thumbnails/blog.jpg')
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
