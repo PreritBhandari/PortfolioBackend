ecfff006d82ece667ce09406f1e4d54421efab72
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-10 18:55:52 +0545

    [Updated] permission classes

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import ListCreateAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Blog
 from .serializers import ListCreateSerializer
@@ -10,13 +9,11 @@ from .serializers import ListCreateSerializer
 class ListCreateBlog(ListCreateAPIView):
     serializer_class = ListCreateSerializer
     queryset = Blog.objects.all()
-    permission_classes = [IsAuthenticated]
 
 
 class ListByCategory(ListAPIView):
     serializer_class = ListCreateSerializer
     lookup_field = 'category'
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         category = self.kwargs['category']
